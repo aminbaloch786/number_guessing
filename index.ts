4e874f655987bad2e93b511c3b9dd28d5ba231fa
aminbaloch786 <161073085+aminbaloch786@users.noreply.github.com>
2024-04-20 12:32:22 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,27 @@
+#! /usr/bin/env node
+import inquirer from 'inquirer';
+
+async function numberGuessingGame() {
+    const systemGeneratedNo = Math.floor(Math.random() * 10) + 1;
+
+    const answers = await inquirer.prompt([
+        {
+            type: "number",
+            name: "userGuess",
+            message: "Write your guess between 1 to 10: "
+        }
+    ]);
+
+    const { userGuess } = answers;
+
+    console.log(userGuess, "userGuess", systemGeneratedNo);
+
+    if (userGuess === systemGeneratedNo) {
+        console.log(userGuess, "userGuess", systemGeneratedNo, 'SYs');
+        console.log("Yeaaaa Your answer is correct \n You Win!");
+    } else {
+        console.log("Please try again Better luck next time!");
+    }
+}
+
+numberGuessingGame();
